15bbb8da23187a07094c1687941367545b817a6f
Mohammed Sufiyan <mohammedsufiyanas123@gmail.com>
2023-06-25 12:33:28 +0530

    text decoration compleated

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -8,31 +8,52 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text("Automatic water Irrigation"),
+        title: const Text(" WATER IRRIGATION"),
+        centerTitle: true,
         backgroundColor: Colors.transparent,
       ),
-      body: const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            "Bla bla",
+          const Customtile(leadingText: "Moisture :", trailingtext: "20"),
+          const Customtile(leadingText: "Temperature :", trailingtext: "30"),
+          const Customtile(leadingText: "Humidity :", trailingtext: "40"),
+          //  const Spacer(),
+          const SizedBox(
+            height: 40,
           ),
-          Text(
-            "Bla bla",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "Bla bla",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "Bla bla",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "Bla bla",
-            style: TextStyle(color: Colors.white),
-          ),
+          MaterialButton(
+            color: Colors.white,
+            onPressed: () {},
+            child: const Text("History", style: const TextStyle(fontSize: 19)),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class Customtile extends StatelessWidget {
+  const Customtile({
+    super.key,
+    required this.leadingText,
+    required this.trailingtext,
+  });
+  final leadingText;
+  final trailingtext;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: ListTile(
+        leading: Text(
+          leadingText,
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        trailing: Text(
+          trailingtext,
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+        ),
       ),
     );
   }
